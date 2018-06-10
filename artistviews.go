45fc401b55e0be08b1b5959ce7f0611e9b28194c
alokmatta <alokmatta@gmail.com>
2018-06-11 00:14:48 +0100

    Update artistviews.go

--- artistviews.go
+++ artistviews.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
+	
+	"github.com/dgryski/go-topk"
 )
 
 // SimpleChaincode example simple Chaincode implementation
@@ -14,7 +16,11 @@ type ArtistViewsChaincode struct {
 
 //var A, B string
 //var Aval, Bval, X int
-var Count int = 0
+//var Count int = 0
+k := flag.Int("n", 10, "k")
+tk := topk.New(*k)
+var top10 [10]string
+var newTop10 [10]string
 
 // Init callback representing the invocation of a chaincode
 // This chaincode will manage two accounts A and B and will transfer X units from A to B upon invoke
@@ -25,10 +31,47 @@ func (t *ArtistViewsChaincode) Init(stub shim.ChaincodeStubInterface) pb.Respons
 
 func (t *ArtistViewsChaincode) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	fmt.Println("ArtistViewsChaincode Invoke Called")
-	fmt.Print("Artist Count ")
-	Count = Count + 1
-	fmt.Print(Count)
+	//fmt.Print("Artist Count ")
+	//Count = Count + 1
+	//fmt.Print(Count)
+	item := args[0]
+	tk.Insert(item, 1)
+	
+	KeysArray := tk.Keys()
+	
+	if len(top10) > 0 {
+			if top10[0] != KeysArray[0].Key {
+				fmt.Println("New number 1 - " + KeysArray[0].Key)
+			}
+		}
+
+		for i, v := range KeysArray {
+
+			if !contains(v.Key, top10) {
+				fmt.Println("New Artist in top 10 - " + v.Key)
+			}
+
+			newTop10[i] = KeysArray[i].Key
+
+		}
+		top10 = newTop10
+		fmt.Print("Current Top Ten ")
+		fmt.Println(top10)
+	
+		for _, v := range tk.Keys() {
+		fmt.Println(v.Key, v.Count, v.Error)
+	}
+	
 	return shim.Success(nil)
+}
+
+func contains(e string, s [10]string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
 
 func (t *ArtistViewsChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
