0f6024e536a231c6ccfde0464233b57a71979762
alokmatta <alokmatta@gmail.com>
2018-06-10 23:41:22 +0100

    Update artistviews.go

--- artistviews.go
+++ artistviews.go
@@ -14,6 +14,7 @@ type ArtistViewsChaincode struct {
 
 //var A, B string
 //var Aval, Bval, X int
+var Count int = 0
 
 // Init callback representing the invocation of a chaincode
 // This chaincode will manage two accounts A and B and will transfer X units from A to B upon invoke
@@ -23,7 +24,10 @@ func (t *ArtistViewsChaincode) Init(stub shim.ChaincodeStubInterface) pb.Respons
 }
 
 func (t *ArtistViewsChaincode) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	fmt.Println("Invoke Called")
+	fmt.Println("ArtistViewsChaincode Invoke Called")
+	fmt.Print("Artist Count ")
+	count = count + 1
+	fmt.Print(count)
 	return shim.Success(nil)
 }
 
